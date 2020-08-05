57f5216cc4328a2ca01956e58f0147602822e374
Martín Schere <tinschere@gmail.com>
2020-08-05 09:13:12 -0300

    GDAL and GEOS

--- backend/settings.py
+++ backend/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
 }
 
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
